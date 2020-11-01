603a64e8a8d69871966ae38166fdf58ed81ec154
Adam Norbäcker <24547930+adamnorbacker@users.noreply.github.com>
2020-11-01 22:31:05 +0100

    Fix: Update now works with official reshade structure

--- Tools/download-shaders.ps1
+++ Tools/download-shaders.ps1
@@ -1,13 +1,16 @@
 #----------------------FUNCTIONS BEGIN HERE-------------------------
 
 function extractShaders {
-	foreach ($file in Get-ChildItem .\temp238698092) {
-		if (($file.Name -match '\.fx') -or ($file.name -match '\.cfg')) {
-			if (($user -eq 2) -AND (Test-Path -path ".\reshade-shaders\Shaders\$file" -PathType Leaf)) {
-				Move-Item -Path $file.FullName -Destination .\temp238698092\Shaders
+	foreach ($file in Get-ChildItem -Path ".\reshade-shaders\Shaders" –Recurse) {
+		$currentDirectory = (Get-ChildItem $file.FullName -Directory)
+		$fileName = $file.Name
+		if (($fileName -match '\.fx') -or ($fileName -match '\.cfg')) {
+			if (($option -eq 2) -AND (Test-Path -path ".\temp238698092\$fileName" -PathType Leaf)) {
+				# Write-Output $file
+				Copy-Item -Path .\temp238698092\$fileName -Destination $currentDirectory -Force
 			}
-			elseif ($user -eq 1) {
-				Move-Item -Path $file.FullName -Destination .\temp238698092\Shaders
+			elseif ($option -eq 1) {
+				Copy-Item -Path .\temp238698092\$fileName -Destination $currentDirectory -Force
 			}
 		}	  
 	}
@@ -16,10 +19,10 @@ function extractShaders {
 function extractTextures {
 	foreach ($file in Get-ChildItem .\temp238698092) {
 		if (($file.Name -match '\.png') -or ($file.name -match '\.dds') -or ($file.name -match '\.bmp') -or ($file.name -match '\.jp*g')) {
-			if (($user -eq 2) -AND (Test-Path -path ".\reshade-shaders\Shaders\$file" -PathType Leaf)) {
+			if (($option -eq 2) -AND (Test-Path -path ".\reshade-shaders\Shaders\$file" -PathType Leaf)) {
 				Move-Item -Path $file.FullName -Destination .\temp238698092\Textures
 			}
-			elseif ($user -eq 1) {
+			elseif ($option -eq 1) {
 				Move-Item -Path $file.FullName -Destination .\temp238698092\Textures
 			}
 		}  
@@ -29,8 +32,8 @@ function extractTextures {
 #----------------------FUNCTIONS END HERE----------------------------
 
 #Create reshade-shaders directory
-[int]$user = Read-Host -Prompt "1 = Update existing and install new shaders | 2 = Update existing shaders"
-if (($user -ne 1) -and ($user -ne 2)) {
+[int]$option = Read-Host -Prompt "1 = Update existing and install new shaders | 2 = Update existing shaders"
+if (($option -ne 1) -and ($option -ne 2)) {
 	Read-Host -Prompt "Invalid input, press enter to exit script"
 	Exit
 }
@@ -40,11 +43,11 @@ if (!(Test-Path -path "reshade-shaders\Shaders" -PathType Container)) { New-Item
 if (!(Test-Path -path "reshade-shaders\Textures" -PathType Container)) { New-Item -Path ".\" -Name "reshade-shaders\Textures" -ItemType "directory" }
 
 #Ask user if they want to download all shaders or select repositories
-[int]$user1 = 1
-if ($user -eq 1) {
+[int]$option1 = 1
+if ($option -eq 1) {
  #if user just wants to update shaders, don't prompt for repos
-	$user1 = Read-Host -Prompt "1 = Download all repos | 2 = Select repos to download"
-	if (($user1 -ne 1) -and ($user1 -ne 2)) {
+	$option1 = Read-Host -Prompt "1 = Download all repos | 2 = Select repos to download"
+	if (($option1 -ne 1) -and ($option1 -ne 2)) {
 		Read-Host -Prompt "Invalid input, press enter to exit script"
 		Exit
 	}
@@ -57,24 +60,24 @@ if ($user -eq 1) {
 #Download repositories
 $client = new-object System.Net.WebClient
 Write-Output "Downloading respositories, please wait"
-if ($user1 -eq 2) { Write-Output "Select repos to download: 1 = Yes | 0 = No" }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "qUINT") -eq 1)) { $client.DownloadFile("https://github.com/martymcmodding/qUINT/archive/master.zip", ".\temp238698092\quint.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Crosire") -eq 1)) { $client.DownloadFile("https://github.com/crosire/reshade-shaders/archive/slim.zip", ".\temp238698092\slim.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Crosire Legacy") -eq 1)) { $client.DownloadFile("https://github.com/crosire/reshade-shaders/archive/master.zip", ".\temp238698092\legacy.zip") }
+if ($option1 -eq 2) { Write-Output "Select repos to download: 1 = Yes | 0 = No" }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "qUINT") -eq 1)) { $client.DownloadFile("https://github.com/martymcmodding/qUINT/archive/master.zip", ".\temp238698092\quint.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Crosire") -eq 1)) { $client.DownloadFile("https://github.com/crosire/reshade-shaders/archive/slim.zip", ".\temp238698092\slim.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Crosire Legacy") -eq 1)) { $client.DownloadFile("https://github.com/crosire/reshade-shaders/archive/master.zip", ".\temp238698092\legacy.zip") }
 else { $legacy = 0 }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "SweetFX") -eq 1)) { $client.DownloadFile("https://github.com/CeeJayDK/SweetFX/archive/master.zip", ".\temp238698092\sweetfx.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "prod80") -eq 1)) { $client.DownloadFile("https://github.com/prod80/prod80-ReShade-Repository/archive/master.zip", ".\temp238698092\prod80.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Depth3D") -eq 1)) { $client.DownloadFile("https://github.com/BlueSkyDefender/Depth3D/archive/master.zip", ".\temp238698092\depth3d.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "AstrayFX") -eq 1)) { $client.DownloadFile("https://github.com/BlueSkyDefender/AstrayFX/archive/master.zip", ".\temp238698092\astrayfx.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "OtisFX") -eq 1)) { $client.DownloadFile("https://github.com/FransBouma/OtisFX/archive/master.zip", ".\temp238698092\otisfx.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Pirate") -eq 1)) { $client.DownloadFile("https://github.com/Heathen/Pirate-Shaders/archive/master.zip", ".\temp238698092\pirate.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Brussell1") -eq 1)) { $client.DownloadFile("https://github.com/brussell1/Shaders/archive/master.zip", ".\temp238698092\brussell1.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Daodan317081") -eq 1)) { $client.DownloadFile("https://github.com/Daodan317081/reshade-shaders/archive/master.zip", ".\temp238698092\daodan.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Fubax") -eq 1)) { $client.DownloadFile("https://github.com/Fubaxiusz/fubax-shaders/archive/master.zip", ".\temp238698092\fubax.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "FXShaders") -eq 1)) { $client.DownloadFile("https://github.com/luluco250/FXShaders/archive/master.zip", ".\temp238698092\fxshaders.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "SweetFX") -eq 1)) { $client.DownloadFile("https://github.com/CeeJayDK/SweetFX/archive/master.zip", ".\temp238698092\sweetfx.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "prod80") -eq 1)) { $client.DownloadFile("https://github.com/prod80/prod80-ReShade-Repository/archive/master.zip", ".\temp238698092\prod80.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Depth3D") -eq 1)) { $client.DownloadFile("https://github.com/BlueSkyDefender/Depth3D/archive/master.zip", ".\temp238698092\depth3d.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "AstrayFX") -eq 1)) { $client.DownloadFile("https://github.com/BlueSkyDefender/AstrayFX/archive/master.zip", ".\temp238698092\astrayfx.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "OtisFX") -eq 1)) { $client.DownloadFile("https://github.com/FransBouma/OtisFX/archive/master.zip", ".\temp238698092\otisfx.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Pirate") -eq 1)) { $client.DownloadFile("https://github.com/Heathen/Pirate-Shaders/archive/master.zip", ".\temp238698092\pirate.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Brussell1") -eq 1)) { $client.DownloadFile("https://github.com/brussell1/Shaders/archive/master.zip", ".\temp238698092\brussell1.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Daodan317081") -eq 1)) { $client.DownloadFile("https://github.com/Daodan317081/reshade-shaders/archive/master.zip", ".\temp238698092\daodan.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Fubax") -eq 1)) { $client.DownloadFile("https://github.com/Fubaxiusz/fubax-shaders/archive/master.zip", ".\temp238698092\fubax.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "FXShaders") -eq 1)) { $client.DownloadFile("https://github.com/luluco250/FXShaders/archive/master.zip", ".\temp238698092\fxshaders.zip") }
 else { $FXShaders = 0 }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Radegast FFXIV") -eq 1)) { $client.DownloadFile("https://github.com/Radegast-FFXIV/reshade-shaders/archive/master.zip", ".\temp238698092\radegast.zip") }
-if (($user1 -eq 1) -or ((Read-Host -Prompt "Lunacy") -eq 1)) { $client.DownloadFile("https://github.com/LordOfLunacy/Insane-Shaders/archive/master.zip", ".\temp238698092\lunacy.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Radegast FFXIV") -eq 1)) { $client.DownloadFile("https://github.com/Radegast-FFXIV/reshade-shaders/archive/master.zip", ".\temp238698092\radegast.zip") }
+if (($option1 -eq 1) -or ((Read-Host -Prompt "Lunacy") -eq 1)) { $client.DownloadFile("https://github.com/LordOfLunacy/Insane-Shaders/archive/master.zip", ".\temp238698092\lunacy.zip") }
 
 #Unzip all archives, delete zips, and move all folders that should be preserved
 if ($legacy -eq 1) {
@@ -91,7 +94,7 @@ Remove-Item .\temp238698092\*.zip
 #Maintain Exceptions
 #Handle FXShaders requirements
 if ($FXShaders -eq 1) {
-	if ((!(Test-Path -path "reshade-shaders\Shaders\FXShaders" -PathType Container)) -and ($user -eq 1) ) {
+	if ((!(Test-Path -path "reshade-shaders\Shaders\FXShaders" -PathType Container)) -and ($option -eq 1) ) {
 		New-Item -Path ".\" -Name "reshade-shaders\Shaders\FXShaders" -ItemType "directory"
 	}
 	extractShaders
@@ -111,16 +114,15 @@ if (!(Test-Path -path "reshade-shaders\LICENSES" -PathType Container)) { New-Ite
 foreach ($file in Get-ChildItem .\temp238698092 -Recurse) {
 	if ($file.Name -match 'LICENSE') {
 		$licensenum++
-		if (($user -eq 2) -AND (Test-Path -path ".\reshade-shaders\Shaders\$file" -PathType Leaf)) {
+		if (($option -eq 2) -AND (Test-Path -path ".\reshade-shaders\Shaders\$file" -PathType Leaf)) {
 			Rename-Item -Path $file.FullName -NewName "LICENSE$licensenum"
 			$newdir = $file.FullName + $licensenum
 			Move-Item -Path $newdir -Destination .\reshade-shaders\LICENSES -Force
 		}
-		elseif ($user -eq 1) {
+		elseif ($option -eq 1) {
 			Rename-Item -Path $file.FullName -NewName "LICENSE$licensenum"
 			$newdir = $file.FullName + $licensenum
 			Move-Item -Path $newdir -Destination .\reshade-shaders\LICENSES -Force
-            
 		}
 	}  
 }
@@ -130,18 +132,9 @@ Get-ChildItem .\temp238698092 -Recurse | `
 	Where-Object { $_.PSIsContainer -eq $False } | `
 	ForEach-Object { Move-Item -Path $_.FullName -Destination .\temp238698092 -Force }
 
-#Move shaders to shaders folder, move textures to textures folder, delete remaining garbage
-New-Item -Path ".\temp238698092" -Name "Shaders" -ItemType "directory"
-New-Item -Path ".\temp238698092" -Name "Textures" -ItemType "directory"
-
 extractShaders
 extractTextures
 
-Remove-Item .\temp238698092\*.*
-
-#Move to reshade-shaders
-Move-Item -Path ".\temp238698092\Shaders\*" -Destination .\reshade-shaders\Shaders -Force
-Move-Item -Path ".\temp238698092\Textures\*" -Destination .\reshade-shaders\Textures -Force
-Remove-Item .\temp238698092 -Recurse
+Remove-Item '.\temp238698092' -Recurse
 
 Read-Host -Prompt "Shaders successfully installed/updated.  Press Enter to exit"
